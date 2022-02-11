441a40e25bbb199be8d06f01f830cfde05d6a757
fabio-bonarota <95079013+fabio-bonarota@users.noreply.github.com>
2022-02-11 12:27:25 +0100

    Add files via upload

---
+++ AgentMaps-gh-pages/demos/simple/street_features.js
@@ -0,0 +1,119 @@
+var streets_data = {
+              "type": "FeatureCollection",
+              "features": [
+              {
+              "type": "Feature",
+              "id": "way/252847532",
+              "properties": {
+                  "timestamp": "2017-12-29T04:00:36Z",
+                  "version": "3",
+                  "changeset": "54999725",
+                  "user": "Chen Jia",
+                  "uid": "288524",
+                  "highway": "tertiary",
+                  "id": "way/252847532"
+              },
+              "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                      [
+                          116.7241561,
+                          39.7852721
+                      ],
+                      [
+                          116.7146487,
+                          39.7853138
+                      ],
+                      [
+                          116.7011945,
+                          39.7854046
+                      ],
+                      [
+                          116.7003805,
+                          39.7854042
+                      ],
+                      [
+                          116.6946918,
+                          39.7854012
+                      ]
+                  ]
+              }
+          },
+          {
+              "type": "Feature",
+              "id": "way/549667173",
+              "properties": {
+                  "timestamp": "2017-12-29T04:00:35Z",
+                  "version": "1",
+                  "changeset": "54999725",
+                  "user": "Chen Jia",
+                  "uid": "288524",
+                  "highway": "service",
+                  "id": "way/549667173"
+              },
+              "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                      [
+                          116.7003617,
+                          39.7841249
+                      ],
+                      [
+                          116.7003805,
+                          39.7854042
+                      ]
+                  ]
+              }
+          },
+          {
+              "type": "Feature",
+              "id": "way/549667186",
+              "properties": {
+                  "timestamp": "2017-12-29T05:10:00Z",
+                  "version": "2",
+                  "changeset": "55000369",
+                  "user": "Chen Jia",
+                  "uid": "288524",
+                  "highway": "tertiary",
+                  "id": "way/549667186"
+              },
+              "geometry": {
+                  "type": "LineString",
+                  "coordinates": [
+                      [
+                          116.6916645,
+                          39.7777828
+                      ],
+                      [
+                          116.6944532,
+                          39.7777757
+                      ],
+                      [
+                          116.6986455,
+                          39.7777592
+                      ],
+                      [
+                          116.6997499,
+                          39.7777491
+                      ],
+                      [
+                          116.7011426,
+                          39.7777365
+                      ],
+                      [
+                          116.7011265,
+                          39.7796185
+                      ],
+                      [
+                          116.7011473,
+                          39.7819021
+                      ],
+                      [
+                          116.7011945,
+                          39.7854046
+                      ]
+                  ]
+              }
+          }
+      ]
+  }
